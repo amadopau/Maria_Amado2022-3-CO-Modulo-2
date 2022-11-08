284ea56cb6396692c59e891c78ec0c87e3a3afd7
amadopau <pau_amado03@hotmail.com>
2022-11-08 12:01:16 -0500

    2 commit
    commit homework 1 ducking

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,12 +1,13 @@
 import pygame
 from pygame.sprite import Sprite
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 class Dinosaur(Sprite):
   X_POS = 80
   Y_POS = 310
   JUMP_SPEED = 8.5
+  DUCK_SPEED = 10
 
   def __init__(self):
     self.image = RUNNING[0]
@@ -14,15 +15,19 @@ class Dinosaur(Sprite):
     self.dino_rect.x = self.X_POS
     self.dino_rect.y = self.Y_POS
     self.step_index = 0
+    self.duck_speed = 10
     self.jump_speed = 8.5
     self.dino_run = True
     self.dino_jump = False
+    self.dino_duck = False
 
   def update(self, user_input):
     if self.dino_run:
         self.run()
     elif self.dino_jump:
         self.jump()
+    elif self.duck:
+        self.duck()
 
     if user_input[pygame.K_UP] and not self.dino_jump:
         self.dino_jump = True
@@ -30,6 +35,10 @@ class Dinosaur(Sprite):
     elif not self.dino_jump:
         self.dino_jump = False
         self.dino_run = True
+
+    elif user_input[pygame.K_DOWN]:
+        self.dino_duck = True
+        self.dino_run = False
 
     if self.step_index >= 10:
         self.step_index = 0
@@ -43,7 +52,7 @@ class Dinosaur(Sprite):
 
   def jump(self):
     self.image = JUMPING
-    self.dino_rect.y -= self.jump_speed *4
+    self.dino_rect.y -= self.jump_speed * 4
 
     self.jump_speed -= 0.5
 
@@ -51,6 +60,16 @@ class Dinosaur(Sprite):
         self.dino_rect.y = self.Y_POS
         self.dino_jump = False
         self.jump_speed = self.JUMP_SPEED
+    
+  def duck(self):
+    self.image = DUCKING[0] if self.step_index > 5 else DUCKING[1]
+    self.dino_rect.y -= self.duck_speed * 4
+    self.duck_speed -= 0.5
+
+    if self.duck_speed < -self.DUCK_SPEED:
+        self.dino_rect.y = self.Y_POS
+        self.dino_duck = False
+        self.duck_speed = self.DUCK_SPEED
 
   def draw(self, screen):
     screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
